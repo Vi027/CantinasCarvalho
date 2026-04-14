9b54855ef0f5fdbacf29654e715135e7524d793e
Vitoria Leite 👩‍💻 <leitevitoria027@gmail.com>
2026-04-14 16:10:08 -0300

    Add files via upload

---
+++ esqueleto/script.js
@@ -0,0 +1,7 @@
+const botão = document.querySelector("#botao-tema")
+const body = document.querySelector("body")
+ 
+botao.addEventListener("click", () => {
+    body.classList.toggle("tema-escuro");
+});
+ 
